d1e28b55181f79ce255d8c43c855d835318855b9
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2021-03-27 03:17:16 -0300

    Types and Roles

--- web/src/interfaces/Protocol.ts
+++ web/src/interfaces/Protocol.ts
@@ -1,9 +1,15 @@
 import {User} from './User'
 import {Department} from './Department'
 
+enum typeProtocol {
+  protocolo,
+  lei,
+}
+
 export type Protocol = {
   id: number
   title: string
+  type: typeProtocol
   description: string
   user: User
   persona: User
